1b0e4ad1f89c8a0c814457aa120c77ccf78309f1
dongnanzhy <dongnanzhy@gmail.com>
2016-02-20 23:17:43 -0500

    eval

--- seq2seq_NER/preprocess/dataUtl.py
+++ seq2seq_NER/preprocess/dataUtl.py
@@ -1,4 +1,4 @@
-
+from sklearn.metrics import f1_score
 
 def eval_helper(file_path):
     with open(file_path, "r") as myfile:
@@ -11,12 +11,11 @@ def eval_helper(file_path):
                 if len(seq) < 1:
                     continue
                 text = ' '.join(seq)
-                with open("input_sentence", "a") as myfile:
+                with open("input_sentence_train", "a") as myfile:
                     myfile.write(text + "\n")
-                with open("label_eval", "a") as myfile:
+                with open("label_eval_train", "a") as myfile:
                     for l in label:
                         myfile.write(l + "\n")
-                    myfile.write("\n")
                 seq = []
                 label = []
             else:
@@ -24,5 +23,17 @@ def eval_helper(file_path):
                 seq = seq+[val[0]]
                 label = label + [val[1]]
 
+def eval (file_true, file_predict):
+    true_label = []
+    pred_label = []
+    with open(file_true, "r") as myfile:
+        for line in myfile:
+            true_label.append(line.strip())
+    with open(file_predict, "r") as myfile:
+        for line in myfile:
+            pred_label.append(line.strip())
+    return f1_score(true_label, pred_label, average='macro')
+
 if __name__ == "__main__":
-    eval_helper()
+    # eval_helper("../ner/train")
+    print eval ("label_eval_train", "label_eval_train")
